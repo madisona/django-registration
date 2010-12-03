0f7736b31b610e14d0f413822bfbc458ff8cc867
Aaron Madison <aaron.madison@theimtgroup.com>
2010-12-03 16:06:28 -0600

    added django 1.3 to setup dependency list

--- setup.py
+++ setup.py
@@ -4,13 +4,16 @@ from setuptools import setup
 setup(
     name="django-registration",
     version="0.1",
-    description="Django app using buildout",
+    description="Django registration app using buildout",
     author="Aaron Madison",
-
+    zip_safe = True,
     package_dir={'': 'src'},
+    packages=('registration',),
+    include_package_data=True,
     install_requires = (
-#        'django==1.2.3',
+        'django==1.3-alpha-1',
         'unittest2',        # for tests
         'mock',              # for tests
     ),
+    dependency_links = ('http://www.djangoproject.com/download/1.3-alpha-1/tarball/#egg=django-1.3-alpha-1',),
 )
