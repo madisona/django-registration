b67f32d314a775ba4760e0308183f874c1af6ca8
Aaron <aaron.l.madison@gmail.com>
2010-12-10 21:55:14 -0600

    added a get_site function so we can get the site whether the sites module is installed or not

--- src/registration/__init__.py
+++ src/registration/__init__.py
@@ -1,3 +1,6 @@
+
+from django.contrib.sites import models as site_models
+
 VERSION = (0, 0, 1)
 
 def get_version():
@@ -5,3 +8,10 @@ def get_version():
     if VERSION[2]:
         version = '%s.%s' % (version, VERSION[2])
     return version
+
+def get_site(request):
+    if site_models.Site._meta.installed:
+        site = site_models.Site.objects.get_current()
+    else:
+        site = site_models.RequestSite(request)
+    return site
