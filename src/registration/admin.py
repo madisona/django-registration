68a93d9cbbe888f60de70813f5f66f7ea2cc1114
Aaron <aaron.l.madison@gmail.com>
2010-12-10 21:55:37 -0600

    changed admin to use new get_site method

--- src/registration/admin.py
+++ src/registration/admin.py
@@ -1,9 +1,8 @@
 
 from django.contrib import admin
-from django.contrib.sites.models import RequestSite
-from django.contrib.sites.models import Site
 
 from registration import models
+from registration import get_site
 
 class RegistrationAdmin(admin.ModelAdmin):
     actions = ('activate_users', 'resend_activation_email')
@@ -12,19 +11,11 @@ class RegistrationAdmin(admin.ModelAdmin):
     search_fields = ('user__username', 'user__first_name')
 
     def activate_users(self, request, queryset):
-        'activates selected users'
         for profile in queryset:
             models.RegistrationProfile.objects.activate_user(profile.activation_key)
 
-    def _get_site(self, request):
-        if Site._meta.installed:
-            site = Site.objects.get_current()
-        else:
-            site = RequestSite(request)
-
     def resend_activation_email(self, request, queryset):
-        'resends activation email for selected users'
-        site = self._get_site(request)
+        site = get_site(request)
         for profile in queryset:
             if not profile.activation_key_expired():
                 profile.send_activation_email(site)
