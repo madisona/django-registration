676ede31072a9a7dd8c64cf0aa40dac58b557fe1
Aaron Madison <aaron.l.madison@gmail.com>
2011-03-20 07:56:23 -0500

    changed setup file

--- setup.py
+++ setup.py
@@ -3,17 +3,11 @@ from setuptools import setup
 
 setup(
     name="django-registration",
-    version="0.0.1",
-    description="Django registration app using buildout",
+    version="0.0.2",
+    description="Django registration app",
     author="Aaron Madison",
     #zip_safe = True,
-    package_dir={'': 'src'},
+    package_dir={'': 'registration'},
     #packages=('registration',),
     include_package_data=True,
-    install_requires = (
-        'django==1.3-alpha-1',
-        'unittest2',        # for tests
-        'mock',              # for tests
-    ),
-    dependency_links = ('http://www.djangoproject.com/download/1.3-alpha-1/tarball/#egg=django-1.3-alpha-1',),
 )
