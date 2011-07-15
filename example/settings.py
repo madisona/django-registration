6a2dd4516f984e77be1d79ee5d9d94c7e095d9af
Aaron Madison <aaron.l.madison@gmail.com>
2011-07-14 19:53:28 -0500

    added example project

---
+++ example/settings.py
@@ -0,0 +1,69 @@
+# Django settings for example project.
+
+# app lives in a directory above our example
+# project so we need to make sure it is findable on our path.
+import sys
+from os.path import abspath, dirname, join
+parent = abspath(dirname(__file__))
+grandparent = abspath(join(parent, '..'))
+for path in (grandparent, parent):
+    if path not in sys.path:
+        sys.path.insert(0, path)
+        
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'example.db',
+    }
+}
+
+
+SITE_ID = 1
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '18wfdr=88@^qh)8itkac#jb^q8&h&rt5+*c-q^h91$pc$xo+7)'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+ROOT_URLCONF = 'example.urls'
+
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+
+    'registration',
+)
+
+ACCOUNT_ACTIVATION_DAYS = 7
