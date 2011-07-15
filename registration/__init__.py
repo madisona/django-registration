5ff698e794d0d01cf9b00160ff36a009ec26bcca
Aaron Madison <aaron.l.madison@gmail.com>
2011-07-14 19:49:58 -0500

    made setup.py test  work... bumped version

--- registration/__init__.py
+++ registration/__init__.py
@@ -1,7 +1,7 @@
 
 from django.contrib.sites import models as site_models
 
-VERSION = (0, 0, 1)
+VERSION = (0, 1, 0)
 
 def get_version():
     version = '%s.%s' % (VERSION[0], VERSION[1])
--- setup.py
+++ setup.py
@@ -1,13 +1,33 @@
 
+import os
 from setuptools import setup
 
+TEST_REQUIREMENTS = [
+    'django>=1.3',
+    'mock',
+]
+
+README = os.path.join(os.path.dirname(__file__), 'README')
 setup(
     name="django-registration",
-    version="0.0.2",
-    description="Django registration app",
+    version="0.1.0",
     author="Aaron Madison",
-    #zip_safe = True,
-    package_dir={'': 'registration'},
-    #packages=('registration',),
+    description="Django registration app",
+    url="https://github.com/madisona/django-registration",
+    test_suite='runtests.runtests',
+    packages=('registration',),
     include_package_data=True,
+    tests_require=TEST_REQUIREMENTS,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+    ],
+    zip_safe = False,
 )
